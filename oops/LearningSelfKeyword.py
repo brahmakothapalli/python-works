870f326a96a5006481163fea365fab900217a251
brahma457 <brahma457@gmail.com>
2022-10-04 21:55:30 +0530

    updated venv

---
+++ oops/LearningSelfKeyword.py
@@ -0,0 +1,13 @@
+class master:
+    
+    def servent(self):
+        self.name = "A1"
+        print(self.name)
+
+    def servent2(self):
+        print(self.name)
+
+obj = master()
+obj.servent()
+obj.servent2()
+# master.servent(obj)
