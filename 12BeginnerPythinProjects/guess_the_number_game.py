e89788fa63dc0e689d61b397992e2d85e5f6aa9c
brahma457 <brahma457@gmail.com>
2022-10-06 19:10:20 +0530

    guess the number and madlibs projects added

---
+++ 12BeginnerPythinProjects/guess_the_number_game.py
@@ -0,0 +1,15 @@
+import random
+
+def guess_number(n):
+    random_number = random.randint(1, n)
+    my_guess = 0
+    while my_guess!=random_number:
+         my_guess = int(input('enter your gussing number: '))
+         if (my_guess > random_number):
+            print("Sorry, Your guess is too high!! Try again.")
+         elif(my_guess < random_number):
+            print("Sorry, Your guess is too low!! Try again.")
+    print("Yayy!! your guess is correct!!!!!")
+
+
+guess_number(10)
---
+++ 12BeginnerPythinProjects/madlibs.py
@@ -0,0 +1,5 @@
+adj1 = input("adjective1 : ")
+tool = input("tool name: ")
+conf = input("conf name: ")
+madlibs = f"I like Python programming {adj1}!, I satrted using it with {tool}. I also attended {conf} in Hyderabad. I keep learning Python, not sure where it is going to land."
+print(madlibs)
