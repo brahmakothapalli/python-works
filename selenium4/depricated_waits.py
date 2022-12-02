32a4f84193d74ca8d661036a73bdb65501891877
brahmakothapalli <brahma457@gmail.com>
2022-12-02 11:47:37 +0530

    selenium 4 changes

---
+++ selenium4/depricated_waits.py
@@ -0,0 +1,9 @@
+import imp
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService
+
+def set_up():
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+    driver.maximize_window()
+    driver.get("https://www.google.com")
